a0d0789d749350dcc9c7c58be6925ea42ae48aed
barthaleventegabor <b.levi.levi@gmail.com>
2026-03-19 10:12:06 +0100

    gege

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,31 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import {  RouterLink, RouterOutlet } from '@angular/router';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    TranslateModule
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('panda');
+
+  translateService = inject(TranslateService);
+
+  translate!: TranslateService
+
+  ngOnInit() {
+    this.translate = this.translateService;
+    this.translate.use('hu');
+  }
+
+  changeLanguage(lang: Event): void {
+    this.translate.use((lang.target as HTMLSelectElement).value);
+  }
+
 }
