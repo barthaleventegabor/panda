a1fcaf7ec78c155f9c3081554ad6edc48326d720
barthaleventegabor <b.levi.levi@gmail.com>
2026-03-19 11:09:35 +0100

    kész

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-product',
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss',
 })
